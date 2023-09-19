2a010cd23e5324db40da377abc2b9d0f5263350b
niceguykhj01 <haejuneiss@gmail.com>
2023-09-19 04:55:53 +0000

    completed ex00

--- Module00/ex00/megaphone.cpp
+++ Module00/ex00/megaphone.cpp
@@ -2,7 +2,6 @@
 #include <string>
 #include <cctype>
 #include <cstring>
-#include <algorithm>
 
 int main(int ac, char *av[])
 {
@@ -10,10 +9,16 @@ int main(int ac, char *av[])
     std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
     }
 
-    for (int i = 1; i < ac; i++) {
-        std::transform(av[i], av[i] + (sizeof(char) * strlen(av[i])), av[i], toupper);
-        std::cout << av[i];
+    for (int i = 1; i < ac; i ++) {
+        for (int j = 0; j < int(strlen(av[i])); j++) {
+                char ch = toupper(av[i][j]);
+                std::cout << ch;
+        }
     }
+    
+    
+
+    
     std::cout << std::endl;
     return (0);
 }
