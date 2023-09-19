01f2b40492fdb0fc5cf601d260869fedd4288ae1
niceguykhj01 <haejuneiss@gmail.com>
2023-09-19 05:43:33 +0000

    started ex01

---
+++ Module00/ex01/phonebook.cpp
@@ -0,0 +1,5 @@
+#include "phonebook.hpp"
+
+int main() {
+    
+}
---
+++ Module00/ex01/phonebook.hpp
@@ -0,0 +1,15 @@
+class Contact {
+    public:
+        char* first;
+        char* last;
+        char* nickname;
+        char* login;
+        char* postal;
+        char* email;
+        char* phone;
+        char* birthday;
+        char* meal;
+        char* underwear;
+        char* secret;
+
+};
